ef7890ce3bb45bd4231dfd3a0fba60cabd177cd2
Preston <preston4896@me.com>
2020-06-21 21:09:42 -0700

    Code is ok, but i have to train the bot.

--- app/nlp.js
+++ app/nlp.js
@@ -31,61 +31,42 @@ function responseHandler(nlp_entities, message, user) {
         entities.push(nlp_entities.entities[keys]);
     }
 
-    console.log("Intents: \n", util.inspect(intent, false, null, true /* enable colors */));
-
     // Step 4: Respond appropriately based on intent.
     if ((intent.length > 0) && (intent[0].confidence >= confidence_threshold)) {
-        // greet the users - only if the user gets my name correctly. 
+        let acceptableName = ["preston", "preston ong", "presbot", "preston ong liat sheng", "preston liat sheng ong", "王列聖", "王列圣", "列聖", "列圣", "dude", "bro", "man", "chinito"];
+
+        // check name first.
+        if ((entities.length != 0) && (entities.includes(nlp_entities.entities["name:name"]))) {
+            // check name
+            let name = nlp_entities.entities["name:name"][0].body.toLowerCase();
+            console.log("Verify input name: " + name);
+            if (!acceptableName.includes(name)) {
+                console.log("Wrong name!");
+                return {
+                    "text": "Lmao! Do you even know my name? Let's try this again. :P"
+                }
+            }
+        }
+
+        // name accepted.
+        // greet the users
         if (intent[0].name == "greet") {
             let greetTrait = nlp_entities.traits["wit$greetings"];
-            let acceptableName = ["preston", "preston ong", "presbot", "preston ong liat sheng", "preston liat sheng ong", "王列聖", "王列圣", "列聖", "列圣"];
             if ((traits.includes(greetTrait)) && checkConfidence(greetTrait)) {
-                if ((entities.length != 0) && (entities.includes(nlp_entities.entities["name:name"]))) {
-                    // check name
-                    let name = nlp_entities.entities["name:name"][0].body.toLowerCase();
-                    console.log("Verify input name: " + name);
-                    if (!acceptableName.includes(name)) {
-                        console.log("Wrong name!");
-                        return {
-                            "text": "Lmao! Do you even know my name? Let's try this again. :P"
+                return {
+                    "text": `What's up, ${user.first_name}! It's good to see you. 😎`,
+                    "quick_replies": [
+                        {
+                            "content_type": "text",
+                            "title": "Let's talk!",
+                            "payload": "talk"
+                        },
+                        {
+                            "content_type": "text",
+                            "title": "Goodbye!",
+                            "payload": "bye"
                         }
-                    }
-                    else {
-                        return {
-                            "text": `What's up, ${user.first_name}! It's good to see you. 😎`,
-                            "quick_replies": [
-                                {
-                                    "content_type": "text",
-                                    "title": "Let's talk!",
-                                    "payload": "talk"
-                                },
-                                {
-                                    "content_type": "text",
-                                    "title": "Goodbye!",
-                                    "payload": "bye"
-                                }
-                            ]
-                        }
-                    }
-                }
-
-                // respond to users (strangers) who do not know my name.
-                else {
-                    return {
-                        "text": `Hello, ${user.first_name}! Enchanté! We should be friends! :)`,
-                        "quick_replies": [
-                            {
-                                "content_type": "text",
-                                "title": "Let's talk more!",
-                                "payload": "talk"
-                            },
-                            {
-                                "content_type": "text",
-                                "title": "Goodbye!",
-                                "payload": "bye"
-                            }
-                        ]
-                    }
+                    ]
                 }
             }
         }
@@ -95,10 +76,16 @@ function responseHandler(nlp_entities, message, user) {
                 "text": "Ok. Goodbye! 👋"
             }
         }
+
+        else if ((intent[0].name == "compliment")) {
+            return {
+                "text": "Thanks! You are awesome too! :)"
+            }
+        }
     }
 
     // out-of-scope message.
-    return {
+    else return {
         "text": `Hey, ${user.first_name}. As much as I love to discuss "${message}" with you. I am not the perfect bot yet. Maybe you should talk to human Preston Ong!`,
         "quick_replies": [
             {
